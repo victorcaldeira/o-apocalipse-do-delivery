1b3bb290dcd57a570a44a1bf87c21e13ee784c8a
Victor Caldeira Menezes <victormenn@gmail.com>
2026-06-25 20:55:21 -0300

    test: adiciona validacao de email do pedido - red

---
+++ tests/unit/PedidoValidator.test.js
@@ -0,0 +1,20 @@
+﻿const { PedidoValidator } = require('../../src/validation/PedidoValidator');
+
+describe('PedidoValidator', () => {
+  test('deve rejeitar pedido com e-mail inválido', () => {
+    const validator = new PedidoValidator();
+
+    const pedido = {
+      clienteEmail: 'email-invalido',
+      valor: 150,
+      cartao: {
+        numero: '4111111111111111',
+        validade: '12/30',
+        cvv: '123'
+      }
+    };
+
+    expect(() => validator.validar(pedido))
+      .toThrow('E-mail do cliente inválido');
+  });
+});
