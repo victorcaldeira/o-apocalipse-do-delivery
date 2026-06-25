2f7941d0b4a6aa15472943ccacbdf9316f9e06a1
Victor Caldeira Menezes <victormenn@gmail.com>
2026-06-25 20:56:02 -0300

    feat: implementa validacao de email do pedido - green

---
+++ src/validation/PedidoValidator.js
@@ -0,0 +1,20 @@
+﻿class PedidoValidator {
+  validar(pedido) {
+    const email = pedido?.clienteEmail;
+
+    if (
+      typeof email !== 'string' ||
+      !email.includes('@') ||
+      email.startsWith('@') ||
+      email.endsWith('@')
+    ) {
+      throw new Error('E-mail do cliente inválido');
+    }
+
+    return true;
+  }
+}
+
+module.exports = {
+  PedidoValidator
+};
