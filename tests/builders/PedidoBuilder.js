0c744c76ac234aeb84959c0ce30da30718b9b4e1
Victor Caldeira Menezes <victormenn@gmail.com>
2026-06-25 21:00:03 -0300

    test: amplia validacao do pedido com data builder - red

---
+++ tests/builders/PedidoBuilder.js
@@ -0,0 +1,57 @@
+﻿class PedidoBuilder {
+  constructor() {
+    this.pedido = {
+      clienteEmail: 'cliente@entregasja.com',
+      valor: 150,
+      cartao: {
+        numero: '4111111111111111',
+        validade: '12/30',
+        cvv: '123'
+      },
+      status: 'PENDENTE'
+    };
+  }
+
+  comEmail(clienteEmail) {
+    this.pedido.clienteEmail = clienteEmail;
+    return this;
+  }
+
+  comValor(valor) {
+    this.pedido.valor = valor;
+    return this;
+  }
+
+  comCartao(cartao) {
+    this.pedido.cartao = cartao;
+    return this;
+  }
+
+  semEmail() {
+    delete this.pedido.clienteEmail;
+    return this;
+  }
+
+  semValor() {
+    delete this.pedido.valor;
+    return this;
+  }
+
+  semCartao() {
+    delete this.pedido.cartao;
+    return this;
+  }
+
+  build() {
+    return {
+      ...this.pedido,
+      cartao: this.pedido.cartao
+        ? { ...this.pedido.cartao }
+        : this.pedido.cartao
+    };
+  }
+}
+
+module.exports = {
+  PedidoBuilder
+};
--- tests/unit/PedidoValidator.test.js
+++ tests/unit/PedidoValidator.test.js
@@ -1,20 +1,83 @@
 ﻿const { PedidoValidator } = require('../../src/validation/PedidoValidator');
+const { PedidoBuilder } = require('../builders/PedidoBuilder');
 
 describe('PedidoValidator', () => {
-  test('deve rejeitar pedido com e-mail inválido', () => {
-    const validator = new PedidoValidator();
+  let validator;
 
-    const pedido = {
-      clienteEmail: 'email-invalido',
-      valor: 150,
-      cartao: {
-        numero: '4111111111111111',
-        validade: '12/30',
-        cvv: '123'
-      }
-    };
+  beforeEach(() => {
+    validator = new PedidoValidator();
+  });
+
+  test('deve aceitar um pedido válido', () => {
+    const pedido = new PedidoBuilder().build();
+
+    expect(validator.validar(pedido)).toBe(true);
+  });
+
+  test.each([
+    [undefined],
+    [null],
+    ['email-invalido'],
+    ['cliente@dominio'],
+    ['@dominio.com'],
+    ['cliente@.com']
+  ])('deve rejeitar e-mail inválido: %p', (email) => {
+    const pedido = new PedidoBuilder()
+      .comEmail(email)
+      .build();
 
     expect(() => validator.validar(pedido))
       .toThrow('E-mail do cliente inválido');
+  });
+
+  test.each([
+    [undefined],
+    [null],
+    ['150'],
+    [0],
+    [-1]
+  ])('deve rejeitar valor inválido: %p', (valor) => {
+    const pedido = new PedidoBuilder()
+      .comValor(valor)
+      .build();
+
+    expect(() => validator.validar(pedido))
+      .toThrow('Valor do pedido inválido');
+  });
+
+  test.each([
+    [undefined],
+    [null],
+    ['cartao'],
+    [[]]
+  ])('deve rejeitar cartão inválido: %p', (cartao) => {
+    const pedido = new PedidoBuilder()
+      .comCartao(cartao)
+      .build();
+
+    expect(() => validator.validar(pedido))
+      .toThrow('Cartão inválido');
+  });
+
+  test.each([
+    [
+      { validade: '12/30', cvv: '123' },
+      'Número do cartão é obrigatório'
+    ],
+    [
+      { numero: '4111111111111111', cvv: '123' },
+      'Validade do cartão é obrigatória'
+    ],
+    [
+      { numero: '4111111111111111', validade: '12/30' },
+      'CVV do cartão é obrigatório'
+    ]
+  ])('deve rejeitar cartão incompleto', (cartao, mensagem) => {
+    const pedido = new PedidoBuilder()
+      .comCartao(cartao)
+      .build();
+
+    expect(() => validator.validar(pedido))
+      .toThrow(mensagem);
   });
 });
