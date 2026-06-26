c24847c605a5ba7666ad7186100ac2bda58a3dd2
Victor Caldeira Menezes <victormenn@gmail.com>
2026-06-25 21:54:15 -0300

    feat: implementa single flight contra thundering herd - green

---
+++ src/services/PedidoConsultaService.js
@@ -0,0 +1,76 @@
+﻿class PedidoConsultaService {
+  constructor({
+    pedidoRepository,
+    cache,
+    ttlSegundos = 60
+  }) {
+    this.pedidoRepository = pedidoRepository;
+    this.cache = cache;
+    this.ttlSegundos = ttlSegundos;
+    this.consultasEmAndamento = new Map();
+  }
+
+  async buscarPorId(idPedido) {
+    const chaveCache = this.criarChaveCache(idPedido);
+
+    const pedidoEmCache = await this.cache.buscar(
+      chaveCache
+    );
+
+    if (pedidoEmCache) {
+      return pedidoEmCache;
+    }
+
+    if (this.consultasEmAndamento.has(chaveCache)) {
+      return this.consultasEmAndamento.get(
+        chaveCache
+      );
+    }
+
+    const consulta = this.buscarNoBancoEAtualizarCache(
+      idPedido,
+      chaveCache
+    );
+
+    this.consultasEmAndamento.set(
+      chaveCache,
+      consulta
+    );
+
+    try {
+      return await consulta;
+    } finally {
+      this.consultasEmAndamento.delete(
+        chaveCache
+      );
+    }
+  }
+
+  async buscarNoBancoEAtualizarCache(
+    idPedido,
+    chaveCache
+  ) {
+    const pedido =
+      await this.pedidoRepository.buscarPorId(
+        idPedido
+      );
+
+    if (pedido) {
+      await this.cache.salvar(
+        chaveCache,
+        pedido,
+        this.ttlSegundos
+      );
+    }
+
+    return pedido;
+  }
+
+  criarChaveCache(idPedido) {
+    return `pedido:${idPedido}`;
+  }
+}
+
+module.exports = {
+  PedidoConsultaService
+};
