a49d915b411151a52c1696b41d69cb0d73df3982
Victor Caldeira Menezes <victormenn@gmail.com>
2026-06-25 21:12:18 -0300

    feat: implementa timeout no gateway de pagamento - green

---
+++ src/resilience/GatewayPagamentoResiliente.js
@@ -0,0 +1,53 @@
+﻿class GatewayTimeoutError extends Error {
+  constructor(timeoutMs) {
+    super(
+      `Timeout no gateway de pagamento após ${timeoutMs}ms`
+    );
+
+    this.name = 'GatewayTimeoutError';
+    this.code = 'GATEWAY_TIMEOUT';
+    this.timeoutMs = timeoutMs;
+  }
+}
+
+class GatewayPagamentoResiliente {
+  constructor({
+    gatewayPagamento,
+    timeoutMs = 2000
+  }) {
+    this.gatewayPagamento = gatewayPagamento;
+    this.timeoutMs = timeoutMs;
+  }
+
+  async cobrar(valor, cartao) {
+    let identificadorTimeout;
+
+    const chamadaGateway = Promise
+      .resolve()
+      .then(() =>
+        this.gatewayPagamento.cobrar(valor, cartao)
+      );
+
+    const limiteDeTempo = new Promise((_, rejeitar) => {
+      identificadorTimeout = setTimeout(() => {
+        rejeitar(
+          new GatewayTimeoutError(this.timeoutMs)
+        );
+      }, this.timeoutMs);
+    });
+
+    try {
+      return await Promise.race([
+        chamadaGateway,
+        limiteDeTempo
+      ]);
+    } finally {
+      clearTimeout(identificadorTimeout);
+    }
+  }
+}
+
+module.exports = {
+  GatewayPagamentoResiliente,
+  GatewayTimeoutError
+};
