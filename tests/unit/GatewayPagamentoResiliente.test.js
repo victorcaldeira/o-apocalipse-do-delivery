c2fef04028280f761283fc222cf3cd1862384d7c
Victor Caldeira Menezes <victormenn@gmail.com>
2026-06-25 21:11:16 -0300

    test: define timeout do gateway de pagamento - red

---
+++ tests/unit/GatewayPagamentoResiliente.test.js
@@ -0,0 +1,44 @@
+﻿const {
+  GatewayPagamentoResiliente
+} = require('../../src/resilience/GatewayPagamentoResiliente');
+
+describe('GatewayPagamentoResiliente - timeout', () => {
+  beforeEach(() => {
+    jest.useFakeTimers();
+  });
+
+  afterEach(() => {
+    jest.useRealTimers();
+  });
+
+  test('deve interromper a cobrança quando o gateway exceder 2000ms', async () => {
+    const gatewayPagamento = {
+      cobrar: jest.fn(() => new Promise(() => {}))
+    };
+
+    const gatewayResiliente = new GatewayPagamentoResiliente({
+      gatewayPagamento,
+      timeoutMs: 2000
+    });
+
+    const cobranca = gatewayResiliente.cobrar(
+      150,
+      {
+        numero: '4111111111111111',
+        validade: '12/30',
+        cvv: '123'
+      }
+    );
+
+    const verificacao = expect(cobranca)
+      .rejects
+      .toThrow(
+        'Timeout no gateway de pagamento após 2000ms'
+      );
+
+    await jest.advanceTimersByTimeAsync(2000);
+    await verificacao;
+
+    expect(gatewayPagamento.cobrar).toHaveBeenCalledTimes(1);
+  });
+});
